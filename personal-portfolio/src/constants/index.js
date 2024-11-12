f7420cda51be53016409d6d38fd3a7de03c1d2ec
Νίκος Καραμαρούδης <99655065+karamas13@users.noreply.github.com>
2024-11-12 15:59:09 +0200

    Update index.js information

--- personal-portfolio/src/constants/index.js
+++ personal-portfolio/src/constants/index.js
@@ -30,7 +30,7 @@ export const PROJECTS = [
     description:
       "A fully functional photo-gallery website with features like an image uploader based on categories, image delete feature, and user authentication. This project is for an actual client so access to the crud functionalities of the application is not available.",
     technologies: ["React", "CSS", "Tailwind", "Node.js", "Firebase", "Netlify"],
-    link:"https://nikosbrinias-photography.netlify.app/",
+    link:"https://nikosbriniasphotography.com/",
     details:
     "In this project, I developed a dynamic photography website that showcases the client's work across various categories, including ecosystems, individuals, meals, and concerts. Leveraging Firebase for backend services, the site allows the client to seamlessly upload images while ensuring a smooth user experience through responsive design. I implemented authentication features to safeguard the upload process, allowing only users registered withing the database to use the admin control panel. The site also incorporates a robust storage solution, enabling the client to delete their images, ensuring their content remains curated and relevant. With an eye for detail, I focused on creating an intuitive interface that highlights the beauty of photography, making it accessible and engaging for visitors across all devices.",
   },
